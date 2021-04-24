5eb69a79c0d26b8d8219f83301783caa38ac4c15
Uneven Prankster <33995085+GithubPrankster@users.noreply.github.com>
2021-04-24 01:32:11 -0300

    UI work, preparing for menu-based selection of tech

--- src/main/world.gd
+++ src/main/world.gd
@@ -1,9 +1,13 @@
 extends Spatial
 
 onready var theirhp = $ui/enhp
-onready var urhp = $ui/urhp
 
-onready var urmn = $ui/urmn
+onready var health = $ui/health
+onready var health_under = $ui/health_under
+onready var mana = $ui/mana
+onready var mana_under = $ui/mana_under
+
+onready var tweener = $ui/tweener
 
 signal block(res)
 
@@ -11,20 +15,16 @@ signal block(res)
 func _ready():
 	pass # Replace with function body.
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
-
 func _on_enemy_hit(hth):
 	theirhp.text = "their hp: " + str(hth)
 	emit_signal("block", false)
 
 func _on_tomboy_hit(hth):
-	if hth == 0:
-		urhp.queue_free()
-		urmn.queue_free()
-	else:
-		urhp.text = "ur hp: " + str(hth)
+	health.value = hth
+	tweener.interpolate_property(health_under, "value", health_under.value, health.value, 0.6, Tween.TRANS_SINE)
+	tweener.start()
 
-func _on_tomboy_magic(mana):
-	urmn.text = "ur mn: " + str(mana)
+func _on_tomboy_magic(man):
+	mana.value = man
+	tweener.interpolate_property(mana_under, "value", mana_under.value, mana.value, 0.5, Tween.TRANS_SINE)
+	tweener.start()
